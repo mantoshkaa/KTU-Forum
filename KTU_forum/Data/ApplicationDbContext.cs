8305c4deb9ed2eb849201e52b62d7ab2e6327e80
Mantas Nekrošius <mantas.nekrosius@benify.com>
2025-03-23 22:03:00 +0200

    Add UserModel and ApplicationDbContext for EF Core setup

---
+++ KTU_forum/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KTU_forum.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<UserModel> Users { get; set; }
+    }
+}
---
+++ KTU_forum/Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace KTU_forum.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; } //primary key
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
