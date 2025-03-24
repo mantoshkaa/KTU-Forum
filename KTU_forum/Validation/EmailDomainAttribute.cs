bc178e841c57f3a9c8ab9851d47e4683e1f5daac
Mantas Nekrošius <mannek1@ktu.lt>
2025-03-24 21:48:31 +0200

    Add validation logic in EmailDomainAttribute class.

--- KTU_forum/Validation/EmailDomainAttribute.cs
+++ KTU_forum/Validation/EmailDomainAttribute.cs
@@ -1,6 +1,25 @@
-﻿namespace KTU_forum.Validation
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace KTU_forum.Validation
 {
-    public class Class
+    public class EmailDomainAttribute : ValidationAttribute
     {
+        private readonly string _allowedDomain;
+
+        public EmailDomainAttribute(string allowedDomain)
+        {
+            _allowedDomain = allowedDomain;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var email = value as string;
+            if (email != null && email.EndsWith("@" + _allowedDomain, StringComparison.OrdinalIgnoreCase))
+            {
+                return ValidationResult.Success;
+            }
+            return new ValidationResult($"Email must be from the @{_allowedDomain} domain.");
+        }
     }
 }
