66f77de879be1621b31a8ed75df00fe6403b114e
Taree007 <gintarejasiuk@gmail.com>
2025-04-08 00:48:59 +0300

    added a forgot pasworrd page (without backend)

---
+++ KTU_forum/Pages/ForgotPassword.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KTU_forum.Pages
+{
+    public class ForgotPasswordModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
